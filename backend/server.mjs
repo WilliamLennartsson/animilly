890536e720c005e9e2d839d3dc20d40fa1ac9295
WilliamLennartsson <william.j.lennartsson@gmail.com>
2021-06-05 10:17:38 +0200

    cors, whitelist, state mock

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,16 +1,15 @@
 import express from "express";
 import http from "http";
-// import path from "path";
+import cors from "cors";
 import { Server } from "socket.io";
 import setupMongoose from "./db/setup.mjs";
 import userRoute from "./routes/userRoute.mjs";
 import galleryRoute from "./routes/galleryRoute.mjs";
 const app = express();
 const server = http.createServer(app);
+// TODO: RedisIO for multiple synced servers
 
-// This needs to be defined because of es6 module file format (.mjs)
-// const __dirname = path.resolve(path.dirname(""));
-const PORT = 3000; // <- add env
+const PORT = process.env.PORT || 3000; // <- add env
 
 const io = new Server(server, {
   cors: {
@@ -18,13 +17,21 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-// io.set("transports", ["websocket"]);
 
-// Only for testing io
-// app.use(express.static(path.join(__dirname, "public")));
+const allowlist = ["http://localhost:5000"];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 // Middlewares
-app.use(express.json());  
+app.use(express.json());
+app.use(cors({ origin: allowlist[0] })); // TODO: Fix corsDelegate
 // TODO: Helmet?
 
 // Routes
@@ -33,6 +40,19 @@ app.use("/galleries", galleryRoute);
 
 // Mongoose
 setupMongoose();
+
+const state = {
+  "98123102930okok": {
+    players: [
+      {
+        username: "Pelle",
+        position: { x: 0, y: 0, z: 0 },
+        rotation: 0,
+        actions: ["MOVE_LEFT", "JUMP"],
+      },
+    ],
+  },
+};
 
 // Sockets
 io.on("connection", (socket) => {
@@ -54,3 +74,13 @@ io.on("connection", (socket) => {
 server.listen(PORT, () => {
   console.log("listening on *:3000");
 });
+
+const joinRoom = (player, roomId) => {};
+
+const authMiddleware = (socket, next) => {
+  const handshakeData = socket.request;
+  console.log("IO auth middleware:", handshakeData._query["token"]);
+  next();
+};
+
+io.use(authMiddleware);
