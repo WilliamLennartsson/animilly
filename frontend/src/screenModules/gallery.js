969bfd3b4377926a9db2b203a81306bf05c674e3
WilliamLennartsson <william.j.lennartsson@gmail.com>
2021-06-11 21:19:58 +0200

    gallery module

--- frontend/src/screenModules/gallery.js
+++ frontend/src/screenModules/gallery.js
@@ -1,8 +1,10 @@
-
-import GalleryApp from '../gallerySrc/galleryApp.js'
-import GameClient from "../multiplayer/client.js";
+import GalleryApp from "../gallerySrc/galleryApp.js";
+import GameClient from "../multiplayer/serverClient.js";
+import AuthClient from "../auth/authClient.js";
 
 window.onload = () => {
-  // GalleryApp()
-  const client = new GameClient();
-}
+  GalleryApp()
+  const client = new GameClient();  
+  const authClient = new AuthClient();
+  console.log(`authClient.retreiveAuthToken()`, authClient.retreiveAuthToken());
+};
