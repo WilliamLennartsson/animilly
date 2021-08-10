6d6315db0aae648937d2592db108175c2752d623
WilliamLennartsson <william.j.lennartsson@gmail.com>
2021-08-10 13:42:07 +0200

    utils for random seed and bellCurve

---
+++ frontend/src/utils/bellCurve.js
@@ -0,0 +1,28 @@
+export function randomBellCurve() {
+  let u = 0,
+    v = 0;
+  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randomBellCurve(); // resample between 0 and 1
+  return num;
+}
+
+export function randomBellCurveSkew(min, max, skew) {
+  let u = 0,
+    v = 0;
+  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) num = randomBellCurveSkew(min, max, skew);
+  // resample between 0 and 1 if out of range
+  else {
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
+  }
+  return num;
+}
